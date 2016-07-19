f741975d6b5414ffbafe7b248786d2b643ff426d
weitaolee <12666954@qq.com>
2016-07-19 10:17:10 +0800

    first commit

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COrleans.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("~/aboutus")]
+        public ActionResult AboutUs()
+        {
+
+            return View();
+        }
+    }
+}
